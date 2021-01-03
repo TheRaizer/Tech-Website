fb55d1ec123d39af2a5e12db363ecb1fec370d81
Aidan Fu <aidan.fu000@gmail.com>
2021-01-03 13:10:06 -0500

    added parts icons in grid display

--- src/Components/PartsChoices.js
+++ src/Components/PartsChoices.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import CPUIcon from '../StaticImageFolder/CPU.png';
+import GraphicsCardIcon from '../StaticImageFolder/GraphicsCard.png';
+import RAM from '../StaticImageFolder/RAM.png';
 import './PartsChoices.css';
+
+const icons = [
+  {
+    url: CPUIcon,
+  },
+  {
+    url: GraphicsCardIcon,
+  },
+  {
+    url: RAM,
+  },
+];
+
+icons.forEach((icon, index) => (icon['id'] = index));
 
 function PartsChoices() {
   return (
-    <section className='parts-choices'>
+    <div className='parts-choices'>
       <h1>Choose Your Area</h1>
-    </section>
+      <section className='parts-images'>
+        {icons.map((icon) => {
+          return <img key={icon.id} src={icon.url} alt='unavailable'></img>;
+        })}
+      </section>
+    </div>
   );
 }
 
